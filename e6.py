69bc3b286636ad0c87738b816361791baa8da5e1
andres <aguilar_andres@protonmail.com>
2021-10-17 21:32:25 -0700

    completed string and file exercises

---
+++ e6.py
@@ -0,0 +1,18 @@
+# Write a program that prompts for a file name, then opens that file and reads
+# through the file, looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Use the file name mbox-short.txt as the file name
+
+target = "X-DSPAM-Confidence:"
+total = 0
+value = 0
+
+file = input("Enter file name: ")
+handle = open(file)
+for line in handle:
+    if not line.startswith(target):
+        continue
+    total += 1
+    value += float ( line[line.find('0') : ] )
+
+print("Average spam confidence:", value / total)
