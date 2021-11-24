419748a06f5bda9fb13a5ad0377de6c25c701904
andres <aguilar_andres@protonmail.com>
2021-11-23 18:23:25 -0800

    added assignment instructions as comment to top of file

--- Databases/Many-Students-In-Many-Courses/roster.py
+++ Databases/Many-Students-In-Many-Courses/roster.py
@@ -1,3 +1,7 @@
+"""
+This application will read roster data in JSON format, parse the file, and then produce an SQLite database that contains a User, Course, and Member table and populate the tables from the data file.
+"""
+
 import json
 import sqlite3
 
